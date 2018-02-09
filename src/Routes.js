e84ed4a9b27880eb1389d25f4230aef54fc9ac3b
Rafael Pantoja <rafaelbp92@gmail.com>
2018-02-09 18:53:42 -0300

    Add pagina de boas vindas

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Router, Scene } from 'react-native-router-flux';
 
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
+import Welcome from './components/Welcome';
 
 export default props => (
     <Router>
         <Scene key='root'>
             <Scene key='formLogin' component={FormLogin} title="Login" />
             <Scene key='formCadastro' component={FormCadastro} title="Cadastro" />
+            <Scene key='welcome' component={Welcome} title="Bem Vindo" initial />
         </Scene>
     </Router>
 );
---
+++ src/components/Welcome.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image, ImageBackground, StyleSheet }  from 'react-native'
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+	button: {
+        backgroundColor: '#115E54',
+        paddingVertical: 8,
+        paddingHorizontal: 30,
+        marginTop: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height:  2},
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 3
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',   
+      }
+});
+
+export default props => (
+    <ImageBackground style={{ flex: 1, width: null }} source={require('../imgs/bg.png')}>
+        <View style={{ flex: 1, padding: 15 }}>
+            <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{ fontSize:20, color: '#ffffff' }} >Seja Bem Vindo!</Text>
+                <Image source={ require('../imgs/logo.png') } />
+            </View> 
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Fazer Login</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </ImageBackground>
+);
