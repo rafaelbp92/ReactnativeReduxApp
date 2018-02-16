2363043a0d2b1db9e14e7250d8ee4c4c697a34e9
Rafael Pantoja <rafaelbp92@gmail.com>
2018-02-16 19:19:35 -0300

    Adicionada Tabview

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Principal = props => {
+    return(
     <View>
         <Text>Página principal</Text>
     </View>
+    )
 }
 
 export default Principal;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,43 +1,49 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, Image, ImageBackground, StyleSheet }  from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import * as React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get('window').width,
+};
+
+const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+
+export default class TabViewExample extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
+  render() {
+    return (
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={initialLayout}
+      />
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-	button: {
-        backgroundColor: '#115E54',
-        paddingVertical: 8,
-        paddingHorizontal: 30,
-        marginTop: 20,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height:  2},
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 3
-      },
-      buttonText: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold',   
-      }
+  container: {
+    flex: 1,
+  },
 });
-
-export default props => (
-    <ImageBackground style={{ flex: 1, width: null }} source={require('../imgs/bg.png')}>
-        <View style={{ flex: 1, padding: 15 }}>
-            <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{ fontSize:20, color: '#ffffff' }} >Seja Bem Vindo!</Text>
-                <Image source={ require('../imgs/logo.png') } />
-            </View> 
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={ () => Actions.formLogin() }>
-                    <Text style={styles.buttonText}>Fazer Login</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    </ImageBackground>
-);
